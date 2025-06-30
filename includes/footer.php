0070fd9ac6f0363bc5868d0a52a295b6182ba281
Belle Acedillo <belleacedillo@gmail.com>
2025-06-30 22:19:01 +0800

    update footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <div class="footer-left">© The Cat-alog Library 2025</div>
+    <div class="footer-left">© The Cat-alog Library 2025 | Developed by MingMao</div>
     <div class="footer-right">Version 1.0.0</div>
 </footer>
 <script src="/library-system/assets/js/script.js"></script>
