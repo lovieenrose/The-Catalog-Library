4e0e70a0342160929ed128d22fa4020601ad12a2
Belle Acedillo <belleacedillo@gmail.com>
2025-07-06 15:31:19 +0800

    made same button style in register and added black text outline to all buttons

--- student/register.php
+++ student/register.php
@@ -102,7 +102,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         
         <button type="submit" class="btn">REGISTER</button>
         <a href="../index.php">
-        <button type="button" class="btn">BACK</button>
+            <button type="button" class="btn">BACK</button>
         </a>
     </form>
     
